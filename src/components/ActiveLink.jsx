07e1acc6b529509e47410dcec7188795bf5dc69f
Enrique Adelino <emontes@dstr.net>
2023-10-30 20:33:13 -0500

    Navbar styling

--- src/components/ActiveLink.jsx
+++ src/components/ActiveLink.jsx
@@ -1,16 +1,16 @@
-"use client"
+"use client";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
-
-const style = {textDecoration: 'underline'}
-
-
+const style = { textDecoration: "underline" };
 
 const ActiveLink = ({ text, href }) => {
-
-    const asPath = usePathname()
-  return <Link href={href} style = { asPath === href ? style : null}>{text}</Link>;
+  const asPath = usePathname();
+  return (
+    <Link className="" href={href} style={asPath === href ? style : null}>
+      {text}
+    </Link>
+  );
 };
 
 export default ActiveLink;
