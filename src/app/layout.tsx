f3204e13c515e4ad8449b3fcc2742ffad6895f45
Enrique Adelino <emontes@dstr.net>
2023-10-31 13:46:02 -0500

    add NextUI Library

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Inter } from "next/font/google";
+import { Providers } from "./providers";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
@@ -16,11 +17,17 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-          <Navbar />
-          {children}
-          <Footer />
-        </main>
+        <Providers>
+          
+          <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Navbar />
+            <div>
+
+    </div>
+            {children}
+            <Footer />
+          </main>
+        </Providers>
       </body>
     </html>
   );
