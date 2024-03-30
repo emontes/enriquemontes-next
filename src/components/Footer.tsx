3d5615b94122710556edb2fbe78d95a22a83a346
Enrique Montes <enrique@truey.com>
2024-03-29 22:10:58 -0500

    remove use of styled-components in Footer and make only tailwind

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,150 +1,81 @@
 import socialLinks from "@/app/constants/social_links";
-import styled from "styled-components";
 import Image from "next/image";
-import device from "@/app/device";
 import links from "@/app/constants/links-footer";
 import Link from "next/link";
 
 const Footer = () => {
-  return (
-    <Wrapper>
-      <div className="logo-box">
-        <Image
-          src="/images/logo-eama.png"
-          alt="Enrique Adelino Montes Araujo (EAMA)"
-          className="logo"
-          width={110}
-          height={110}
-        />
+	return (
+		<footer className="bg-grey-1 text-grey-10 dark:bg-grey-10 dark:text-grey-1 py-16">
+			<div className="flex flex-col items-center mb-16">
+				<Image
+					src="/images/logo-eama.png"
+					alt="Enrique Adelino Montes Araujo (EAMA)"
+					className="w-28 h-28"
+					width={110}
+					height={110}
+				/>
 
-        <div className="social-links">
-          {socialLinks.map((link) => {
-            return (
-              <a href={link.url} key={link.id} className="social-link">
-                {link.icon}
-              </a>
-            );
-          })}
-        </div>
-      </div>
-      <div className="footer-bottom">
-        <div className="navigation">
-          <div className="underline" />
-          <ul className="footer__list">
-            {links.map((item) => {
-              return (
-                <li className="footer__item" key={item.url}>
-                  <Link href={item.url} className="link">
-                    {item.text}
-                  </Link>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
+				<div className="mt-8 flex space-x-4">
+					{socialLinks.map((link) => {
+						return (
+							<a
+								href={link.url}
+								key={link.id}
+								className="text-primary-7 dark:text-primary-6 hover:text-grey-8 transition-all duration-300 text-4xl hover:-translate-y-4"
+							>
+								{link.icon}
+							</a>
+						);
+					})}
+				</div>
+			</div>
 
-        <div className="copy">
-          <div className="underline" />
-          <p className="capitalize">
-            copyright &copy; {new Date().getFullYear()}{" "}
-            <span>Enrique Adelino Montes Araujo</span> all rights reserved
-          </p>
-        </div>
-      </div>
-      <div className="mt-4 flex items-center justify-center text-xl">
-        Site made with: 
-      <Image
-          className="relative ml-3 invert dark:invert-0 drop-shadow-[0_0_0.3rem_#ffffff70]"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={80}
-          height={37}
-        />
-      </div>
-    </Wrapper>
-  );
+			<div className="flex flex-col md:flex-row justify-between items-center">
+				<div className="navigation mb-8 md:mb-0 px-4">
+					<div className="underline" />
+					<ul className="flex flex-wrap">
+						{links.map((item) => {
+							return (
+								<li
+									key={item.url}
+									className="mr-6 mb-4 hover:rotate-[-5deg] hover:scale-110 transition-all duration-300"
+								>
+									<Link
+										href={item.url}
+										className="uppercase  hover:text-primary-5  hover:scale-75 transition-all duration-300"
+									>
+										{item.text}
+									</Link>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+
+				<div className="copy px-4">
+					<div className="underline" />
+					<p className="capitalize">
+						copyright &copy; {new Date().getFullYear()}{" "}
+						<span className=" text-primary-5">
+							Enrique Adelino Montes Araujo
+						</span>{" "}
+						all rights reserved
+					</p>
+				</div>
+			</div>
+
+			<div className="mt-12 flex items-center justify-center text-3xl">
+				Site made with:
+				<Image
+					className="relative ml-6 invert dark:invert-0 drop-shadow-[0_0_0.3rem_#ffffff70] animate-bounce"
+					src="/next.svg"
+					alt="Next.js Logo"
+					width={80}
+					height={37}
+				/>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
-
-const Wrapper = styled.footer`
-  color: var(--clr-grey-10);
-  background-color: var(--clr-grey-1);
-  padding: 5rem 0;
-
-  .logo-box {
-    margin-bottom: 4rem;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .logo {
-    width: 8rem;
-    height: auto;
-  }
-
-  .social-links {
-    margin-top: 2rem;
-  }
-  .social-link {
-    color: var(--clr-primary-7);
-    &:hover {
-      color: var(--clr-grey-9);
-    }
-  }
-
-  .footer-bottom {
-    display: flex;
-    flex-direction: column;
-
-    @media ${device.tablet} {
-      flex-direction: row;
-
-      gap: 20%;
-      > * {
-        flex: 1;
-      }
-    }
-  }
-
-  .navigation {
-    padding: 1rem;
-    display: inline-block;
-    text-align: center;
-  }
-
-  .footer__item {
-    display: inline-block;
-
-    &:not(:last-child) {
-      margin-right: 1.5rem;
-    }
-  }
-
-  .link {
-    color: var(--clr-grey-10);
-    background-color: var(--clr-grey-1);
-    text-decoration: none;
-    text-transform: uppercase;
-    display: inline-block;
-    transition: all 0.2s;
-
-    &:hover {
-      color: var(--clr-primary-5);
-      transform: rotate(-5deg) scale(1.3);
-    }
-  }
-
-  .copy {
-    margin: auto;
-    padding: 1rem;
-  }
-
-  p {
-    color: var(--clr-grey-10);
-  }
-
-  span {
-    color: var(--clr-primary-5);
-  }
-`;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,49 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [
-    require("tailwindcss-animate"),
-  ],
-}
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+			},
+			colors: {
+				"primary-1": "var(--clr-primary-1)",
+				"primary-2": "var(--clr-primary-2)",
+				"primary-3": "var(--clr-primary-3)",
+				"primary-4": "var(--clr-primary-4)",
+				"primary-5": "var(--clr-primary-5)",
+				"primary-6": "var(--clr-primary-6)",
+				"primary-7": "var(--clr-primary-7)",
+				"primary-8": "var(--clr-primary-8)",
+				"primary-9": "var(--clr-primary-9)",
+				"primary-10": "var(--clr-primary-10)",
+				"grey-1": "var(--clr-grey-1)",
+				"grey-3": "var(--clr-grey-3)",
+				"grey-4": "var(--clr-grey-4)",
+				"grey-5": "var(--clr-grey-5)",
+				"grey-8": "var(--clr-grey-8)",
+				"grey-9": "var(--clr-grey-9)",
+				"grey-10": "var(--clr-grey-10)",
+				"green-dark": "var(--clr-green-dark)",
+				"red-dark": "var(--clr-red-dark)",
+				"red-light": "var(--clr-red-light)",
+				black: "var(--clr-black)",
+				white: "var(--clr-white)",
+				"white-transparency-8": "var(--clr-white-transparency-8)",
+				"dark-mode": {
+					"grey-1": "rgb(241, 245, 248)",
+					"grey-10": "hsl(209, 61%, 16%)",
+					"primary-1": "#eefbff",
+					"primary-10": "rgb(0, 37, 92)",
+				},
+			},
+		},
+	},
+	plugins: [require("tailwindcss-animate")],
+};
