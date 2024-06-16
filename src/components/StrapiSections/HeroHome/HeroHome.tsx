baf3aaef07ed560cc072b3e53e0172e383064bb4
emontes <emontes@dstr.net>
2024-06-16 15:58:53 -0500

    Adjusts in Hero components

--- src/components/StrapiSections/HeroHome/HeroHome.tsx
+++ src/components/StrapiSections/HeroHome/HeroHome.tsx
@@ -21,25 +21,25 @@ const Hero = (props) => {
       <div className="absolute inset-0 bg-primary-1 opacity-70 z-10" />
 
       {/* Hero Content */}
-      <div className="relative z-20 grid grid-cols-1 lg:grid-cols-2 gap-8 px-8 py-16 lg:py-8 max-w-6xl mx-auto items-center">
+      <div className="relative z-20 grid grid-cols-1 lg:grid-cols-2 gap-8 px-4 py-8 sm:py-12 lg:py-8 max-w-6xl mx-auto items-center">
         {/* Text Content */}
-        <div className="space-y-4 lg:space-y-6">
-          <div className="underline bg-black h-1 w-16 mb-4" />
-          <h1 className="text-5xl font-bold">{props.Header1}</h1>
-          <h3 className="text-2xl mt-2">{props.Header3}</h3>
-          <h4 className="text-xl mt-1 text-grey-5">{props.Header4}</h4>
+        <div className="space-y-4 lg:space-y-6 text-center lg:text-left">
+          <div className="underline bg-black h-1 w-16 mb-4 mx-auto lg:mx-0" />
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl font-bold">{props.Header1}</h1>
+          <h3 className="text-xl sm:text-2xl mt-2">{props.Header3}</h3>
+          <h4 className="text-lg sm:text-xl mt-1 text-grey-5">{props.Header4}</h4>
           <Link href={props.LinkUrl}>
-            <div className="mt-5 inline-block px-6 py-2 bg-primary-5 text-white rounded hover:bg-primary-7 cursor-pointer">
+            <div className="mt-5 inline-block px-4 sm:px-6 py-2 bg-primary-5 text-white rounded hover:bg-primary-7 cursor-pointer">
               {props.LinkText}
             </div>
           </Link>
           {/* Social Links */}
-          <div className="mt-4 flex space-x-6">
+          <div className="mt-4 flex justify-center lg:justify-start space-x-4 sm:space-x-6">
             {socialLinks.map((link) => (
               <a
                 href={link.url}
                 key={link.id}
-                className="text-primary-5 hover:text-primary-7 text-2xl transform transition-transform duration-300 hover:scale-110"
+                className="text-primary-5 hover:text-primary-7 text-xl sm:text-2xl transform transition-transform duration-300 hover:scale-110"
               >
                 {link.icon}
               </a>
@@ -50,14 +50,14 @@ const Hero = (props) => {
         {/* Profile Image */}
         {props.ProfileImage.data && (
           <div className="relative mx-auto lg:mx-0">
-            <div className="relative w-32 h-32 lg:w-40 lg:h-40 overflow-hidden rounded-full border-4 border-white shadow-lg transform hover:scale-105 transition-transform duration-500 ease-in-out">
+            <div className="relative w-24 h-24 sm:w-32 sm:h-32 lg:w-40 lg:h-40 overflow-hidden rounded-full border-4 border-white shadow-lg transform hover:scale-105 transition-transform duration-500 ease-in-out">
               <Image
                 src={props.ProfileImage.data.attributes.formats.medium.url}
                 alt="Profile Image"
                 layout="fill"
                 className="object-cover"
               />
-              <div className="absolute inset-0 bg-gradient-to-br from-primary-5 to-gray-800 opacity-25 rounded-full"></div>
+              <div className="absolute inset-0 bg-gradient-to-br from-primary-5 to-gray-800 opacity-25 rounded-full" />
             </div>
           </div>
         )}
--- src/components/StrapiSections/HeroSection/HeroSection.tsx
+++ src/components/StrapiSections/HeroSection/HeroSection.tsx
@@ -3,21 +3,33 @@ import { HeadingText } from "@/components/HeadingText";
 import Image from "next/image";
 import { Link } from "@/navigation";
 
-export default function HeroSection({ BackgroundImage, SubTitle, Heading, HeroActions, Centered, HeroImage, }: HeroSectionProps) {
-  console.log("Las Hero Actions: ", HeroActions);
+export default function HeroSection({
+  BackgroundImage,
+  SubTitle,
+  Heading,
+  HeroActions,
+  Centered,
+  HeroImage,
+}: HeroSectionProps) {
   return (
     <header className="bg-primary-10">
       <div className="hero relative h-[49vh] bg-primary-1 bg-cover bg-center">
         {BackgroundImage?.data ? (
           <div className="hero-img h-full rounded-lg">
-            <Image src={BackgroundImage.data.attributes.url} role="banner" alt="background gradient" fill priority />
+            <Image
+              src={BackgroundImage.data.attributes.url}
+              role="banner"
+              alt="background gradient"
+              fill
+              priority
+            />
           </div>
         ) : (
           ""
         )}
         <div className="hero-container absolute top-0 left-0 w-full h-full flex items-center justify-center bg-gradient-to-br from-[rgba(0,37,92,0.7)] to-[rgba(199,226,222,0.9)] rounded-lg">
           <div className="header__text-box absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center">
-            <h1 className="heading-primary text-primary-10 text-shadow-[1px_1px_2px_rgba(0,0,0,0.5)] uppercase backface-hidden mb-16 font-light">
+            <div className="heading-primary text-primary-10 text-shadow-[1px_1px_2px_rgba(0,0,0,0.5)] uppercase backface-hidden mb-16 font-light">
               <span className="heading-primary--main block text-4xl tracking-wide animate-moveInLeft">
                 <HeadingText
                   attributes={{ id: "HeroTitle", className: "heading-primary--main" }}
@@ -26,15 +38,15 @@ export default function HeroSection({ BackgroundImage, SubTitle, Heading, HeroAc
                 />
               </span>
               {SubTitle && (
-                <span className="heading-primary--sub mt-4 block text-2xl tracking-wide animate-moveInRight">
+                <span className="mt-4 block tracking-wide animate-moveInRight">
                   <HeadingText
-                    attributes={{ id: "HeroSubTitle", className: "heading-primary--sub" }}
+                    attributes={{ id: "HeroSubTitle", className: "" }}
                     HeadingText={SubTitle.HeadingText}
                     HeadingType={SubTitle.HeadingType}
                   />
                 </span>
               )}
-            </h1>
+            </div>
             {HeroActions
               ? HeroActions.map((props) => (
                   <Link
