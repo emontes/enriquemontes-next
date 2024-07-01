feb1f4f19871d4e8c2c85826b1b9f8fc66a5d7db
emontes <emontes@dstr.net>
2024-06-30 22:50:48 -0500

    feature initial display posts

--- src/app/[locale]/posts/page.tsx
+++ src/app/[locale]/posts/page.tsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { fetchAllPosts } from "@/app/utils/posts";
+import type { PostsListData } from "@/app/utils/posts";
 
-const Posts = () => {
+
+export default async function PostsList({ params: { locale } }: { params: { locale: string } }) {
+  const postsData: PostsListData = await fetchAllPosts(locale);
+
   return (
-    <div>Posts</div>
-  )
+    <div>
+      <h1>Posts</h1>
+      <h2>{locale} Locale</h2>
+      {postsData.data.map((post) => (
+        <div key={post.id}>
+          <h3>{post.attributes.title}</h3>
+          <p>Slug: {post.attributes.slug}</p>
+          <p>Description: {post.attributes.description}</p>
+          <p>Date: {post.attributes.date}</p>
+          {post.attributes.image.data && (
+            <img 
+              src={post.attributes.image.data.attributes.formats.thumbnail.url} 
+              alt={post.attributes.image.data.attributes.alternativeText || post.attributes.title}
+            />
+          )}
+        </div>
+      ))}
+      <div>
+        <h3>Pagination</h3>
+        <p>Page: {postsData.meta.pagination.page}</p>
+        <p>Page Size: {postsData.meta.pagination.pageSize}</p>
+        <p>Page Count: {postsData.meta.pagination.pageCount}</p>
+        <p>Total: {postsData.meta.pagination.total}</p>
+      </div>
+    </div>
+  );
 }
-
-export default Posts
---
+++ src/app/utils/posts.tsx
@@ -0,0 +1,60 @@
+export interface PostsListData {
+    data: {
+		id: number;
+		attributes: {
+			title: string;
+			slug: string;
+			description: string;
+			date: string;
+			image: {
+				data: {
+					id: number;
+					attributes: {
+						name: string;
+						alternativeText: string;
+						url: string;
+						formats: {
+							thumbnail: {
+								url: string;
+							};
+							small: {
+								url: string;
+							};
+							medium: {
+								url: string;
+							};
+						};
+					};
+				};
+			};
+		};
+	}[];
+	meta: {
+		pagination: {
+			page: number;
+			pageSize: number;
+			pageCount: number;
+			total: number;
+		};
+	};
+}   
+
+export const fetchAllPosts = async (lang: string) => {
+	try {
+		const res = await fetch(
+			`${process.env.STRAPI_API_URL}/blog-posts?populate=*&locale=${lang}&sort=date:desc`,
+			{
+				headers: {
+					Authorization: `bearer ${process.env.STRAPI_API_TOKEN}`,
+				},
+			},
+		);
+		const data = await res.json();
+        // console.log('La Data: ', data)
+
+		return data;
+	} catch (error) {
+		console.log("error while fetching Postdata Content from strapi:", error);
+		return {};
+	}
+};
