425a25cc993434b6da735ca0dd2bc5dd5e61a93f
Enrique Adelino Montes Araujo <emontes@dstr.net>
2025-11-12 23:50:37 -0500

    feat: add Apollo tracking script to application

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -51,6 +51,16 @@ export default async function LocaleLayout({
     const messages = await getMessages();
     return (
         <html lang={locale}>
+            <head>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");
+  o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,
+  o.onload=function(){window.trackingFunctions.onLoad({appId:"691556f47cdb83001594924f"})},
+  document.head.appendChild(o)}initApollo();`
+                    }}
+                />
+            </head>
             <body className={inter.className}>
                 <NextIntlClientProvider locale={locale} messages={messages}>
                     <MainLayout
